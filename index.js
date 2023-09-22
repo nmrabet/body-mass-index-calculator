3e009beabb4a648e25fa90bc74c86764d339922d
Nesrine Mrabet <mrabet_nesrine@yahoo.fr>
2023-09-22 19:00:01 +0100

    feat: radio button metric imperial display

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+const radioBtns = document.querySelectorAll("input[name='radio-select']");
+
+const metricView = document.getElementById("metric-view");
+const imperialView = document.getElementById("imperial-view");
+
+function selectMetric() {
+  let selected = document.querySelector(
+    "input[name='radio-select']:checked"
+  ).value;
+  if (selected === "metric") {
+    metricView.style.display = "block";
+    imperialView.style.display = "none";
+  } else {
+    metricView.style.display = "none";
+    imperialView.style.display = "block";
+  }
+}
+
+radioBtns.forEach((radioBtn) => {
+  radioBtn.addEventListener("change", selectMetric);
+});
