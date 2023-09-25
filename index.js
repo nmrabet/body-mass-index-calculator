2709e7a4381ee707051ee91a1c267c8a38873b01
Nesrine Mrabet <mrabet_nesrine@yahoo.fr>
2023-09-25 18:11:12 +0100

    feat: despription bmi and ideal weight

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const imperialView = document.getElementById("imperial-view");
 const metricHeight = document.getElementById("mheight");
 const metricWeight = document.getElementById("mweight");
 const score = document.getElementById("bmi-score");
+const results = document.getElementById("BMI-result");
 
 function selectMetric() {
   let selected = document.querySelector(
@@ -22,7 +23,31 @@ function selectMetric() {
 
 function callMetricBMI() {
   let bmi = Number(metricWeight.value) / Number(metricHeight.value) ** 2;
-  score.innerHTML = bmi.toFixed(1);
+
+  let description = "";
+
+  if (bmi < 18.5) {
+    description = "a bit too thin";
+  } else if (bmi > 18.5 && bmi < 25) {
+    description = "a healthy weight";
+  } else if (bmi > 25 && bmi < 30) {
+    description = "overweight";
+  } else if (bmi > 30) {
+    description = "obese";
+  }
+
+  let idealWeight = 2.2 * bmi + 3.5 * bmi * (Number(metricHeight.value) - 1.5);
+
+  results.innerHTML = `
+  <h6>Your BMI is...</h6>
+  <h2 id="bmi-score">${bmi.toFixed(1)}</h2>    
+  <div>
+    <p>
+      Your BMI suggests you're <span>${description}.</span>
+      Your ideal weight is <span>${idealWeight.toFixed(1)}.</span>
+    </p>
+  </div>
+  `;
 }
 
 metricWeight.addEventListener("keyup", callMetricBMI);
