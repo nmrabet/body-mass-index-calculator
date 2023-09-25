5268cf85a8aa75ca45de30bbf377c59743cb2012
Nesrine Mrabet <mrabet_nesrine@yahoo.fr>
2023-09-25 13:36:16 +0100

    feat: metric bmi calculation

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const radioBtns = document.querySelectorAll("input[name='radio-select']");
 const metricView = document.getElementById("metric-view");
 const imperialView = document.getElementById("imperial-view");
 
+const metricHeight = document.getElementById("mheight");
+const metricWeight = document.getElementById("mweight");
+const score = document.getElementById("bmi-score");
+
 function selectMetric() {
   let selected = document.querySelector(
     "input[name='radio-select']:checked"
@@ -15,6 +19,13 @@ function selectMetric() {
     imperialView.style.display = "block";
   }
 }
+
+function callMetricBMI() {
+  let bmi = Number(metricWeight.value) / Number(metricHeight.value) ** 2;
+  score.innerHTML = bmi.toFixed(1);
+}
+
+metricWeight.addEventListener("keyup", callMetricBMI);
 
 radioBtns.forEach((radioBtn) => {
   radioBtn.addEventListener("change", selectMetric);
