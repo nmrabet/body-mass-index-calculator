5b34b95aa0b9f5840c8de4f0bf96ea497dec013b
Nesrine Mrabet <mrabet_nesrine@yahoo.fr>
2023-09-27 10:14:41 +0100

    feat: imperial metrics

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const imperialView = document.getElementById("imperial-view");
 
 const metricHeight = document.getElementById("mheight");
 const metricWeight = document.getElementById("mweight");
+
+const ftHeight = document.getElementById("ftHeight");
+const inHeight = document.getElementById("inHeight");
+
+const stWeight = document.getElementById("stWeight");
+const lbsWeight = document.getElementById("lbsWeight");
+
 const score = document.getElementById("bmi-score");
 const results = document.getElementById("BMI-result");
 
@@ -32,10 +39,11 @@ function callMetricBMI() {
     description = "a healthy weight";
   } else if (bmi > 25 && bmi < 30) {
     description = "overweight";
-  } else if (bmi > 30) {
+  } else if (bmi > 30 && bmi > 40) {
     description = "obese";
+  } else if (bmi > 40) {
+    description = "morbidly obese";
   }
-
   let idealWeight = 2.2 * bmi + 3.5 * bmi * (Number(metricHeight.value) - 1.5);
 
   results.innerHTML = `
@@ -50,6 +58,35 @@ function callMetricBMI() {
   `;
 }
 
+function callImperialBMI() {
+  let BMI = (Number(stWeight.value * 14 + lbsWeight.value) / Number(ftHeight.value * 12 + inHeight.value) ** 2) * 703;
+
+  let description = "";
+
+  if (BMI < 18.5) {
+    description = "a bit too thin";
+  } else if (BMI > 18.5 && BMI < 25) {
+    description = "a healthy weight";
+  } else if (BMI > 25 && BMI < 30) {
+    description = "overweight";
+  } else if (BMI > 30 && BMI > 40) {
+    description = "obese";
+  } else if (BMI > 40) {
+    description = "morbidly obese";
+  }
+
+  results.innerHTML = `
+  <h6>Your BMI is...</h6>
+  <h2 id="bmi-score">${BMI.toFixed(1)}</h2>    
+  <div>
+    <p>
+      Your BMI suggests you're <span>${description}.</span>
+    </p>
+  </div>
+  `;
+}
+
+lbsWeight.addEventListener("keyup", callImperialBMI);
 metricWeight.addEventListener("keyup", callMetricBMI);
 
 radioBtns.forEach((radioBtn) => {
